e067dc2cd90fb09d06482e3d527de9eb5bf4a729
tangtiff <tiffanyashera0925@gmail.com>
2026-01-21 20:56:21 -0800

    added world
    there’s land and another asset pack

--- player.gd
+++ player.gd
@@ -1,25 +1,27 @@
 extends CharacterBody2D
 
-
-const SPEED = 130.0
+const speed = 130.0
 const JUMP_VELOCITY = -300.0
 
+func _physics_process(delta):
+	player_movement(delta)
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+func player_movement(delta):
+	
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = speed
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -speed 
+		velocity.y = 0 
+	elif Input.is_action_pressed("ui_down"): 
+		velocity.y = speed
+		velocity.x = 0
+	elif Input.is_action_pressed("ui_up"): 
+		velocity.y = -speed
+		velocity.x = 0	
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.x = 0
+		velocity.y =0
+		
 	move_and_slide()
