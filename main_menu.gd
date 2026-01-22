c2a930cc2372700b28aba27f47f64c011f8ae7be
elizabeth alvarez <lizvarez12@gmail.com>
2026-01-21 23:43:02 -0800

    Added intro screen with arrow to game

--- main_menu.gd
+++ main_menu.gd
@@ -3,4 +3,4 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://game.tscn")
+	get_tree().change_scene_to_file("res://IntroScreen.tscn")
