4559097c2498ecba460d6df35d7bfa1ddb66d46b
Parker Develice <packer.pd12@gmail.com>
2026-02-17 12:05:34 -0800

    satisfaction working perhaps

--- resource/character_entries.gd
+++ resource/character_entries.gd
@@ -4,4 +4,5 @@ class_name CharacterEntry
 @export var id: String
 @export var display_name: String
 @export var portrait: Texture2D
+@export var satisfaction: int
 @export_multiline var full_bio: String
