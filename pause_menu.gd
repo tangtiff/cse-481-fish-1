9caf46d8bac3798b1ce8cef75a65df5dd6e364bb
Your Name <lizvarez@cs.washington.edu>
2026-02-18 16:17:53 -0800

    Added 'controls' to pause menu The actual controls page still needs additional elements added (the 'E' icon specifically) with a description

--- pause_menu.gd
+++ pause_menu.gd
@@ -11,3 +11,7 @@ func _on_main_menu_pressed() -> void:
 	#get_tree().paused = false  # optional
 	print("MAIN MENU")
 	get_tree().change_scene_to_file("res://MainMenu.tscn")  # update path
+	
+func _on_controls_pressed()-> void:
+	Engine.time_scale = 1;
+	get_tree().change_scene_to_file("res://controls_menu.tscn")
