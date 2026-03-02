ac8fe0de00f5e66ccb5bf6e037bea114a274a1a2
tangtiff <tiffanyashera0925@gmail.com>
2026-03-01 18:06:16 -0800

    bobbing book

--- journal/profileBook.gd
+++ journal/profileBook.gd
@@ -1,6 +1,7 @@
 extends TextureButton
 
 @export var inside_book_scene: PackedScene
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready():
 	visible = false
@@ -11,8 +12,10 @@ func _ready():
 func _on_first_unlock(_id):
 	visible = true
 	disabled = false
+	animation_player.play("bobbing")
 
 func _on_pressed() -> void:
+	animation_player.stop()
 	if inside_book_scene:
 		var book = inside_book_scene.instantiate()
 		get_tree().root.add_child(book)  # Changed from current_scene to root
